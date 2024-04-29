b3eb9e09ea10491edecaa7f7373092b0f79d334d
tmichoel <11647967+tmichoel@users.noreply.github.com>
2024-04-29 14:32:40 +0200

    initial code commit

--- src/FaSTLMMlight.jl
+++ src/FaSTLMMlight.jl
@@ -1,5 +1,14 @@
 module FaSTLMMlight
 
-# Write your package code here.
+# dependencies
+using LinearAlgebra
+using Statistics
+using Optim
+
+# Code files
+include("fastlmm-fullrank.jl")
+
+# FaST-LMM Exports
+export fastlmm_fullrank, sigma2_mle_fullrank, minus_log_like_fullrank, delta_mle_fullrank, beta_mle_fullrank_lazy, create_covariate_matrix, project_orth_covar, softplus
 
 end
